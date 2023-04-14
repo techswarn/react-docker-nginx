3b0b89aa988701d24bf4d0682ca52d5d90d53c24
techswarn <tech.swarns@gmail.com>
2023-04-14 16:54:13 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import OtherPages from './Components/OtherPages';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+  //   <Router>
+  //     <div className="App">
 
-      <Routes>
-        <Route exact path='/' element={<Fib/>} />
-        <Route exact path='/Otherpage' element={<OtherPages/>} />
-      </Routes>
+  //     <Routes>
+  //       <Route exact path='/' element={<Fib/>} />
+  //       <Route exact path='/Otherpage' element={<OtherPages/>} />
+  //     </Routes>
 
-    </div>
-  </Router>
-
+  //   </div>
+  // </Router>
+  <div>
+    <h1>Hello there</h1>
+  </div>
   );
 }
 
