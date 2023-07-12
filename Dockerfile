cbac55c0eccb0c64d5dea5b8442a93abbb07d287
techswarn <tech.swarns@gmail.com>
2023-07-12 11:49:02 +0530

    code update

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,9 @@ RUN npm install
 COPY . .
 RUN npm run build
 
-FROM nginx
-EXPOSE 3000
-COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
-COPY --from=builder /app/build /usr/share/nginx/html
+CMD [ "npm", "run", "start" ]
+
+# FROM nginx
+# EXPOSE 3000
+# COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
+# COPY --from=builder /app/build /usr/share/nginx/html
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,22 +1,34 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Weather() {
-    const [weather, setWeather] = useState(null)
-    const getWeather = async () => {
-       const data =  await axios.get('http://10.108.166.31:8000/api/v1/weather')
-       console.log(data.data.data)
-       setWeather(data.data.data)
+  const [weather, setWeather] = useState(null);
+  const [load, setLoad] = useState(true);
+  const getWeather = async () => {
+    const data = await axios.get(
+      "http://139.59.49.153:80/kaniko/api/v1/weather"
+    );
+    if (data) {
+      setLoad(false);
     }
-    useEffect(()=>{
-        getWeather()
-    }, [])
-    console.log(weather)
+
+    setWeather(Math.floor(data.data.data));
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
+  console.log(weather);
   return (
     <div className="container">
-        <h2>Weather in Mangalore: {weather}</h2>
+      <div className="card">
+        <h2>
+          {load ? (
+            <h2>Let me find it!</h2>
+          ) : (
+            <h4>Weather in Mangalore: {weather}c</h4>
+          )}
+        </h2>
+      </div>
     </div>
-  )
+  );
 }
